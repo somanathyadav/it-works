f2957b66c893449f61bf964caa94a527e25aff34
Somanath Yadav <62452765+somanathyadav@users.noreply.github.com>
2020-05-21 10:06:59 +0530

    Initial version downloaded form Spring.io

---
+++ src/main/java/com/itworks/app/superfast/SuperfastApplication.java
@@ -0,0 +1,13 @@
+package com.itworks.app.superfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperfastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperfastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itworks/app/superfast/SuperfastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itworks.app.superfast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperfastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
